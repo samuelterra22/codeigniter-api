19ace118b541e24b320cadc49290730c4d322e41
Samuel Terra <samuelterra22@hotmail.com>
2019-12-12 18:17:55 -0300

    Improve main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,25 +11,17 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={ '/' } className="navbar-brand">React CRUD Example</Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link to={ '/' } className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={ '/create' } className="nav-link">Create</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={ '/index' } className="nav-link">Index</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <h2>Welcome to React CRUD Tutorial</h2> <br/>
+          <h2>CRUD de Alunos</h2>
+
+          <div className="form-group">
+            <Link to={ '/index' } type="button"
+                  className="btn btn-primary mr-4">Listar</Link>
+            <Link to={ '/create' } type="button"
+                  className="btn btn-primary mr-4">Adicionar</Link>
+          </div>
+
           <Switch>
+            <Route exact path='/' component={ Index }/>
             <Route exact path='/create' component={ Create }/>
             <Route path='/edit/:id' component={ Edit }/>
             <Route path='/index' component={ Index }/>
